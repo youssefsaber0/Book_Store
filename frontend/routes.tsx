4c7983a7747f2b275192d60d349e46018aa69104
Youssef Saber <yousefsaber.155@gmail.com>
2023-01-03 18:05:34 +0200

    add books

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -1,6 +1,7 @@
 import MainLayout from 'Frontend/views/MainLayout.js';
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import AddBook from './views/add_book/AddBook';
 import SignupView from './views/signup/SignupView';
 
 const LoginView = lazy(async () => import('Frontend/views/login/LoginView.js'));
@@ -22,6 +23,14 @@ const router = createBrowserRouter([
         element: (
           <Suspense fallback={<div>Loading...</div>}>
             <SignupView />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/addBook',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <AddBook />
           </Suspense>
         ),
       },
